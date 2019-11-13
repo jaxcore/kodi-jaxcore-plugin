d57ea3469c85f9f8e862a7f5ef69c75c7822b143
Dan Steinman <dansteinman@gmail.com>
2019-11-12 19:30:28 -0500

    plugin client store

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
 	services: {
-		kodi: require('./kodi-service')
+		kodi: require('./kodi-service'),
+	},
+	stores: {
+		kodi: 'client'
 	},
 	adapters: {
 		kodi: require('./kodi-adapter')
